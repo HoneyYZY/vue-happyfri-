d10cbf9be9aca2aed4f3c9e4b6e99edfff1ecdb5
yang <yangzhiyang101@163.com>
2019-07-25 10:41:56 +0800

    vue 练习，happyfri答题项目

---
+++ src/page/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home-main">
+      <mainBase></mainBase>
+    </div>
+</template>
+
+<script>
+  import mainBase from '@/components/mainBase'
+    export default {
+        name: "home",
+        components:{
+          mainBase:mainBase
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/page/item.vue
@@ -0,0 +1,17 @@
+<template>
+    <mainBase></mainBase>
+</template>
+
+<script>
+  import mainBase from '@/components/mainBase'
+    export default {
+        name: "item",
+      components:{
+        mainBase:mainBase
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
